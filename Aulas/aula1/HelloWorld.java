dec871a564ac4ed248667f87af195ca30ee3b61e
gon-fel <agoncalves.felipe.pessoal@gmail.com>
2025-09-10 16:05:05 -0300

    Primeiras aulas de Java

---
+++ Aulas/aula1/HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
+}
+
+// para declarar uma clase não pública basta remover o "public" da classe.
+
+// Nessa aula falamos sobre a sintaxe básica em Java. Onde a class -> pode ser pública ou não. Que o nome da classe sempre deve começar com letra maiúscula pois, o java é uma linguagem forte do tipo "case sensitive", por tanto, é preciso saber onde e quanto utilizar letras maiúsculas e minúsculas.
+
+// Neste caso: public class HelloWorld { -> essa é a classe, do tipo publica e o nome dela é Hello World.
+
+//Método -> public static void -> ou sejá, todo método é publico estático e vazio. O void significa que o retorno desse método será vazio. 
+
+// No caso dos métodos o nome, que nesse caso é main, sempre será escrito em letra minúscula.
+
+// todo método iremos inserir parâmetros para execução da função e sempre será entre parênteses. E tem uma configuração, sendo o tipo daquele dado como o primeiro a ser informado, no caso "String []" e o parâmetro também vai ter um nome, que nesse caso é 'arg'. Que nesse caso diz o seguinte: o método de entrada será do tipo string com array/vetor de argumentos.
---
+++ Aulas/aula3/Vetores.java
@@ -0,0 +1,23 @@
+public class Vetores {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5}; // Inicialização com valores
+        String[] names = {"Alice", "Bob", "Charlie"}; // Inicialização com valores
+
+        int[] numero = new int[5]; // Inicialização de um vetor com 5 inteiros
+        String[] nomes = new String[3]; // Inicialização de um vetor com 3 strings
+
+        // mostrar
+        System.out.println("Vetor de números: " + numbers);
+
+        // percorrer
+        System.err.println("Valores de vetor usando FOR");
+        for (int i = 0; i < numbers.length; i++){
+            System.err.println("Elementos na posição [" + i + "]:" + numbers[i]);
+
+        }
+              
+
+
+    }
+    
+}
