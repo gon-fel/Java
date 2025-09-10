6b7466d5be0a55ed00a5178bf18ff47d0f7a09b9
gon-fel <agoncalves.felipe.pessoal@gmail.com>
2025-09-10 19:09:17 -0300

    Atividade prática da faculdade
    Nessa atividade precisei criar três variáveis utilizando tipos primitivos em Java. Gerar uma estrutura condicional e manipular as escolhas do sistema.
    Manipular estrutura de repetições arrays e matriz

---
+++ src/Atividade_Pratica/Cadastro_aluno.java
@@ -0,0 +1,54 @@
+package Atividade_Pratica;
+
+public class Cadastro_aluno {
+    public static void main(String[] args) {
+
+        // Declarando as variáveis primitivas
+        int idade = 20;
+        float media = 8.5f;
+        boolean matricula = true;
+
+        // Criei a Estrutura Condicional e validei as informações
+        if (matricula) {
+            System.out.println("Sua matrícula está ativada");
+        }
+        if (media >= 7) {
+            System.out.println("Aprovado");
+        } else {
+            System.out.println("Reprovado");
+        }
+
+    // Estrutura de Repetição e manipulação de laços de repetição
+    for (int n = 0; n < 4; n++){
+        System.out.println("Analisando a nota do " + (n + 1));
+    }
+    // Tratando as exceções
+    int[] numeros = {20, 30, 40};
+    try {
+        System.out.println(numeros[10]);
+
+    } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Ocorreu um erro ao acessar o array: " + e.getMessage());
+        }
+    // Manipulação de array
+    int[] notas = {8, 7, 9, 10};
+    for (int i : notas){
+        System.out.println("Número do array " + i);
+    }
+
+    // Criando e manipulando a matriz
+    int [][] minhaMatriz = {
+        {10, 20, 30},
+        {40, 50, 60}
+    };
+    for (int o = 0; o < minhaMatriz.length; o++ ){
+        for (int p = 0; p < minhaMatriz[o].length; p++) {
+            System.out.print(minhaMatriz[o][p] + " ");
+        }
+        System.out.println();
+      
+
+    }
+    
+}
+}
